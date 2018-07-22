9abf4069be77a8d409111b1621f89206aea95c8e
M <mc.cm.mail@gmail.com>
2018-07-22 02:43:22 +0200

    Overhauled the Parser interface
    Made the Parser more generic. Instead of being hard coded, the help menu is
    generated from the provided options. There is also a uniform way of calling
    functions for all the options.

--- include/Parser.h
+++ include/Parser.h
@@ -7,17 +7,79 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include <array>
+#include <functional>
+#include <string>
 #include <string_view>
+#include <utility>
+#include <vector>
 #include "Rain.h"
+#include "tmatrix.h"
 
 namespace Parser {
-	bool ParseCmdLineArgs(int argc, char *argv[], int &StepsPerSecond, RainProperties &rainProperties);
+	const int MAX_LINE_LENGTH {80};
+	const int SHORT_GAP_PREFIX {2};
+	const int LONG_GAP_PREFIX {6};
+	const std::string SEPARATOR {"  - "};
+
+	enum OptionType { VERSION, HELP, NUMERIC };
+	struct Option {
+		OptionType Type;
+		std::string ShortLiteral;
+		std::string LongLiteral;
+		std::vector<std::string> HelpText;
+		std::function<void(std::string_view, int &, RainProperties &)> ProcessArgument;
+
+		std::string GetUsage() const;
+		std::string GetLiterals() const;
+		bool HasShortLiteral() const { return (ShortLiteral.length() != 0); }
+		bool MatchesArgument (std::string_view argument) const;
+		std::pair<std::string_view, std::string_view> GetPrefixSuffixSplit(std::string_view argument) const;
+	};
+
+	bool StartsWith(std::string_view str, std::string_view prefix);
+	bool ParseCmdLineArgs(int argc, char *argv[], int &stepsPerSecond, RainProperties &rainProperties);
 	void Parse(char c);
+
+	//---VERSION------------------------------------------------------------
 	void PrintVersion();
+	//---HELP---------------------------------------------------------------
 	void PrintUsage(bool full);
-	bool SetStepsPerSecond(std::string_view value, std::string_view option, int &SpepsPerSecond);
+	std::vector<std::string> CreateUsageHeader();
+	std::size_t FindLongestLiteralsLength();
+	void PrintUsageLine(const Option &option, std::size_t longestLiterals);
+	void PrintSpecificOptionType(OptionType type, std::size_t longestLiterals);
+	//---SPTES-PER-SECONDS--------------------------------------------------
+	void SetStepsPerSecond(std::string_view value, int &stepsPerSecond);
 	int ReturnValidNumber(std::string_view value);
+	//----------------------------------------------------------------------
 	void SetRainProperties(RainProperties &rainProperties);
+
+	const std::array Options {
+		Option{
+			VERSION, "", "--version",
+			{ "Output version information and exit "},
+			[](std::string_view, int &, RainProperties &) { PrintVersion(); }
+		},
+		Option{
+			HELP, "", "--help",
+			{ "Display this help and exit "},
+			[](std::string_view, int &, RainProperties &) { PrintUsage(true); }
+		},
+		Option{
+			NUMERIC, "-s", "--steps-per-second",
+			{
+				"Run this many steps per second",
+				"<value> can range from " + std::to_string(MIN_STEPS_PER_SECOND) +
+					" to " + std::to_string(MAX_STEPS_PER_SECOND),
+				"The default value is " + std::to_string(DEFAULT_STEPS_PER_SECOND)
+			},
+			[](std::string_view value, int &stepsPerSecond, RainProperties &)
+			{
+				SetStepsPerSecond(value, stepsPerSecond);
+			}
+		}
+	};
 }
 
 #endif
--- src/Parser.cpp
+++ src/Parser.cpp
@@ -10,37 +10,127 @@
 #include <cstdlib>
 #include <iostream>
 #include "Parser.h"
-#include "tmatrix.h"
 
 namespace Parser {
-	bool ParseCmdLineArgs(int argc, char *argv[], int &StepsPerSecond, RainProperties &rainProperties)
+	std::string Option::GetUsage() const
+	{
+		switch (Type) {
+		case VERSION:
+		case HELP:
+			return "[" + LongLiteral + ']';
+			break;
+		case NUMERIC:
+			if (ShortLiteral != "" && LongLiteral != "") {
+				return "[" + ShortLiteral + "<value> | " + LongLiteral + "=<value>]";
+			} else if (ShortLiteral != "") {
+				return "[" + ShortLiteral + "<value>]";
+			} else {
+				return "[" + LongLiteral + "=<value>]";
+			}
+			break;
+		}
+		return "";
+	}
+
+	std::string Option::GetLiterals() const
+	{
+		switch (Type) {
+		case VERSION:
+		case HELP:
+			return LongLiteral;
+			break;
+		case NUMERIC:
+			if (ShortLiteral != "" && LongLiteral != "") {
+				return ShortLiteral + ", " + LongLiteral + "=<value>";
+			} else if (ShortLiteral != "") {
+				return ShortLiteral;
+			} else {
+				return LongLiteral + "=<value>";
+			}
+			break;
+		}
+		return "";
+	}
+
+	std::pair<std::string_view, std::string_view> Option::GetPrefixSuffixSplit(std::string_view argument) const
+	{
+		std::string literal;
+		if (StartsWith(argument, "--")) {
+			literal = LongLiteral;
+			if (Type == NUMERIC) {
+				literal += '=';
+			}
+		} else {
+			literal = ShortLiteral;
+		}
+
+		if (argument.size() < literal.size()) {
+			return {"", ""};
+		}
+		std::string_view prefix = argument.substr(0, literal.length());
+		std::string_view suffix = argument.substr(literal.length());
+		if (prefix != literal) {
+			// The prefix doesn't match so the split is invalid.
+			return {"", ""};
+		}
+		return {prefix, suffix};
+	}
+
+	bool Option::MatchesArgument (std::string_view argument) const
+	{
+		auto [prefix, suffix] = GetPrefixSuffixSplit(argument);
+		return (prefix != "");
+	}
+
+	bool StartsWith(std::string_view str, std::string_view prefix) {
+		return (str.substr(0, prefix.length()) == prefix);
+	}
+
+	bool ParseCmdLineArgs(int argc, char *argv[], int &stepsPerSecond, RainProperties &rainProperties)
 	{
 		SetRainProperties(rainProperties);
 		for (int i = 1; i < argc; i++) {
 			std::string_view argument {argv[i]};
 
-			if (argument == "--version") {
-				PrintVersion();
-				return false;
-			} else if (argument == "--help") {
-				PrintUsage(true);
-				return false;
-			} else if (argument.substr(0, 2) == "-s") {
-				std::string_view value;
-				if (argument.length() == 2) {
-					value = argv[++i];
-				} else {
-					value = argument.substr(2);
+			bool matched {false};
+			for (const Option &option : Options) {
+				if (!option.MatchesArgument(argument)) {
+					continue;
 				}
+				auto [prefix, suffix] = option.GetPrefixSuffixSplit(argument);
+				matched = true;
+				switch(option.Type) {
+				case VERSION:
+				case HELP:
+					if (suffix != "") {
+						matched = false;
+						break;
+					}
+					option.ProcessArgument(argument, stepsPerSecond, rainProperties);
+					return false;
+					break;
+				case NUMERIC:
+					if (suffix == "") {
+						suffix = argv[++i];
+						if (suffix == nullptr || StartsWith(prefix, "--")) {
+							std::cout << "No value specified for " << argument << '\n';
+							PrintUsage(false);
+							return false;
+						}
+					}
 
-				if (!SetStepsPerSecond(value, "-s", StepsPerSecond)) {
-					return false;
+					try {
+						option.ProcessArgument(suffix, stepsPerSecond, rainProperties);
+					} catch (const std::out_of_range&) {
+						std::cout << "Invalid value '" << suffix;
+						std::cout << "' specified for " << prefix << '\n';
+						std::cout << "Try 'tmatrix --help' for more information." << '\n';
+						return false;
+					}
+					break;
 				}
-			} else if (argument.substr(0, 19) == "--steps-per-second=") {
-				if (!SetStepsPerSecond(argument.substr(19), "--steps-per-second", StepsPerSecond)) {
-					return false;
-				}
-			} else {
+			}
+			if (!matched) {
 				std::cout << "Unknown option: " << argument << '\n';
 				PrintUsage(false);
 				return false;
@@ -58,6 +148,7 @@ namespace Parser {
 		}
 	}
 
+	//---VERSION------------------------------------------------------------
 	void PrintVersion()
 	{
 		std::cout << "tmatrix version " << VERSION_NUMBER << '\n';
@@ -66,42 +157,93 @@ namespace Parser {
 		std::cout << "SPDX-License-Identifier: GPL-2.0-only" << '\n';
 	}
 
+	//---HELP---------------------------------------------------------------
 	void PrintUsage(bool full)
 	{
-		std::cout << "Usage: tmatrix [--version] [--help] [-s<value> | --steps-per-second=<value>]" << '\n';
+		std::vector<std::string> header {CreateUsageHeader()};
+		for (std::string_view line : header) {
+			std::cout << line << '\n';
+		}
+
 		if (full) {
 			std::cout << "Simulates the digital rain effect from The Matrix." << '\n';
 			std::cout << '\n';
-			std::cout << "  -s, --steps-per-second=<value>  - Run this many steps per second" << '\n';
-			std::cout << "                                    <value> can range from ";
-			std::cout << MIN_STEPS_PER_SECOND << " to " << MAX_STEPS_PER_SECOND << '\n';
-			std::cout << "                                    The default value is ";
-			std::cout << DEFAULT_STEPS_PER_SECOND << '\n';
-			std::cout << "      --help                      - Display this help and exit" << '\n';
-			std::cout << "      --version                   - Output version information and exit" << '\n';
+
+			std::size_t longestLiterals {FindLongestLiteralsLength()};
+			for (const Option &option : Options) {
+				if (option.Type != HELP && option.Type != VERSION) {
+					PrintUsageLine(option, longestLiterals);
+				}
+			}
+			// Print help and version at the end.
+			PrintSpecificOptionType(HELP, longestLiterals);
+			PrintSpecificOptionType(VERSION, longestLiterals);
 		}
 	}
 
-	bool SetStepsPerSecond(std::string_view value, std::string_view option, int &StepsPerSecond)
+	std::vector<std::string> CreateUsageHeader()
 	{
-		if (value == nullptr) {
-			std::cout << "No value specified for " << option << '.' << '\n';
-			PrintUsage(false);
-			return false;
+		std::vector<std::string> usage {"Usage: tmatrix"};
+		const int USAGE_START_LENGTH = usage.back().length();
+
+		for (const Option &option : Options) {
+			if (usage.back().length() + 1 + option.GetUsage().length() > MAX_LINE_LENGTH) {
+				usage.emplace_back(USAGE_START_LENGTH, ' ');
+			}
+			usage.back() += ' ' + option.GetUsage();
 		}
 
-		try {
-			StepsPerSecond = ReturnValidNumber(value);
-			if (StepsPerSecond < MIN_STEPS_PER_SECOND ||
-			    StepsPerSecond > MAX_STEPS_PER_SECOND) {
-				throw std::out_of_range("");
+		return usage;
+	}
+
+	std::size_t FindLongestLiteralsLength()
+	{
+		std::size_t longest {0};
+		for (const Option &option : Options) {
+			std::size_t current {option.GetLiterals().length()};
+			current += option.HasShortLiteral() ? SHORT_GAP_PREFIX : LONG_GAP_PREFIX;
+			if (current > longest) {
+				longest = current;
 			}
-		} catch (const std::out_of_range&) {
-			std::cout << "Invalid value '" << value << "' specified for " << option << '.' << '\n';
-			std::cout << "Try 'tmatrix --help' for more information." << '\n';
-			return false;
 		}
-		return true;
+
+		return longest;
+	}
+
+	void PrintUsageLine(const Option &option, std::size_t longestLiterals)
+	{
+		int gapSize {option.HasShortLiteral() ? SHORT_GAP_PREFIX : LONG_GAP_PREFIX};
+		std::string line(gapSize, ' ');
+		line += option.GetLiterals();
+		line += std::string(longestLiterals-line.length(), ' ');
+		line += SEPARATOR;
+		for (std::size_t i = 0; i < option.HelpText.size(); i++) {
+			if (i != 0) {
+				line += std::string(longestLiterals+SEPARATOR.length(), ' ');
+			}
+			line += option.HelpText[i];
+			line += '\n';
+		}
+		std::cout << line;
+	}
+
+	void PrintSpecificOptionType(OptionType type, std::size_t longestLiterals)
+	{
+		for (const Option &option : Options) {
+			if (option.Type == type) {
+				PrintUsageLine(option, longestLiterals);
+			}
+		}
+	}
+
+	//---SPTES-PER-SECONDS--------------------------------------------------
+	void SetStepsPerSecond(std::string_view value, int &stepsPerSecond)
+	{
+		stepsPerSecond = ReturnValidNumber(value);
+		if (stepsPerSecond < MIN_STEPS_PER_SECOND ||
+		    stepsPerSecond > MAX_STEPS_PER_SECOND) {
+			throw std::out_of_range("");
+		}
 	}
 
 	int ReturnValidNumber(std::string_view value)
@@ -114,6 +256,7 @@ namespace Parser {
 		return std::atoi(value.data());
 	}
 
+	//----------------------------------------------------------------------
 	void SetRainProperties(RainProperties &rainProperties)
 	{
 		rainProperties = { {1, 2}, {4, 20}, {4, 9}, {4, 9}, {5, 7} };
