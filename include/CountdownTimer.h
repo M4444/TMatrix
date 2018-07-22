075976afcad0afa09cda20d78b65859aac447bef
M <mc.cm.mail@gmail.com>
2018-07-22 19:38:55 +0200

    Make constructors throw if an invariant can't be established

--- include/CountdownTimer.h
+++ include/CountdownTimer.h
@@ -7,12 +7,18 @@
 #ifndef COUNTDOWN_TIMER_H
 #define COUNTDOWN_TIMER_H
 
+#include <stdexcept>
+
 class CountdownTimer : public Active {
 	int StartingTime;
 	int CurrentTime;
 public:
 	CountdownTimer(int ST) : StartingTime{ST}, CurrentTime{ST} {}
-	CountdownTimer(int ST, int CT) : StartingTime{ST}, CurrentTime{CT} {}
+	CountdownTimer(int ST, int CT) : StartingTime{ST}, CurrentTime{CT} {
+		if (CT > ST) {
+			throw std::invalid_argument("Current time is greater than starting time.");
+		}
+	}
 
 	bool HasExpired() const { return CurrentTime <= 0; }
 
--- include/Range.h
+++ include/Range.h
@@ -7,13 +7,19 @@
 #ifndef RANGE_H
 #define RANGE_H
 
+#include <stdexcept>
+
 template <typename T>
 class Range {
 	T min;
 	T max;
 public:
 	Range() = default;
-	Range(T Min, T Max) : min{Min}, max{Max} {}
+	Range(T Min, T Max) : min{Min}, max{Max} {
+		if (Min > Max) {
+			throw std::range_error("Min is greater than max.");
+		}
+	}
 
 	T GetMin() { return min; }
 	T GetMax() { return max; }
