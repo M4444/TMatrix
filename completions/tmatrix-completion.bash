5fad58dbd4f6708d5d747153ff75ab24721014f3
Oleksii Filonenko <brightone@protonmail.com>
2019-06-04 22:38:53 +0300

    Added bash completion

---
+++ completions/tmatrix-completion.bash
@@ -0,0 +1,33 @@
+# Copyright (C) 2018-2019 Miloš Stojanović
+#
+# SPDX-License-Identifier: GPL-2.0-only
+
+_tmatrix() {
+	local cur prev split
+	_init_completion -s || return
+
+	local colors=(default white gray black red green yellow blue magenta cyan)
+
+	case $prev in
+		--help|--version)
+			return
+			;;
+		--background|-c|--color|-C)
+			COMPREPLY=( $(compgen -W '${colors[*]}' -- "${cur}") )
+			return
+			;;
+		--mode)
+			COMPREPLY=( $(compgen -W 'default dense' -- "${cur}") )
+			return
+			;;
+	esac
+
+	$split && return
+
+	if [[ $cur == -* ]]; then
+		COMPREPLY=( $(compgen -W '$(_parse_help "$1")' -- "$cur") )
+		COMPREPLY+=( $(compgen -W '-s -f -G -g -C -c -t' -- "$cur") )
+		[[ "${COMPREPLY[0]}" == *= ]] && compopt -o nospace
+		return
+	fi
+} && complete -F _tmatrix tmatrix
