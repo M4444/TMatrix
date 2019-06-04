ac964913cd7c00c73ae273bc45c47fe98752a7c9
Oleksii Filonenko <brightone@protonmail.com>
2019-06-04 22:38:35 +0300

    Added zsh completion
    Thanks to eliasrg for the helpful suggestions.

---
+++ completions/tmatrix-completion.zsh
@@ -0,0 +1,16 @@
+#compdef tmatrix
+
+# Copyright (C) 2018-2019 Miloš Stojanović
+#
+# SPDX-License-Identifier: GPL-2.0-only
+
+local colors=(default white gray black red green yellow blue magenta cyan)
+
+_arguments \
+	--mode=-"[Set the mode of the rain]:"\
+"mode:(default dense)" \
+	{-C+,--color=-}"[Set the color of the characters]:"\
+"color:(${colors[*]})" \
+	{-c+,--background=-}"[Set the color of the background]:"\
+"color:(${colors[*]})" \
+	--
